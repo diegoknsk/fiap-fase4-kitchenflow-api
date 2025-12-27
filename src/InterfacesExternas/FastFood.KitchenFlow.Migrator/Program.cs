79a204ace7054f2d76e26a5f5474a60b8d0a90b3
Diego <diego.sa@pricefy.com.br>
2025-12-27 15:15:58 -0300

    fix actions

--- src/InterfacesExternas/FastFood.KitchenFlow.Migrator/Program.cs
+++ src/InterfacesExternas/FastFood.KitchenFlow.Migrator/Program.cs
@@ -1,39 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
-// Este é um migrator básico que será expandido quando o DbContext estiver disponível
+﻿// Este é um migrator básico que será expandido quando o DbContext estiver disponível
 // Por enquanto, apenas valida que a aplicação pode ser executada
 
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables()
-    .Build();
-
-var loggerFactory = LoggerFactory.Create(builder =>
-{
-    builder.AddConsole();
-    builder.SetMinimumLevel(LogLevel.Information);
-});
-
-var logger = loggerFactory.CreateLogger<Program>();
-
-logger.LogInformation("KitchenFlow Migrator iniciado");
+Console.WriteLine("KitchenFlow Migrator iniciado");
 
 try
 {
-    // TODO: Quando o DbContext estiver disponível, descomentar e implementar:
-    // using var scope = serviceProvider.CreateScope();
-    // var dbContext = scope.ServiceProvider.GetRequiredService<KitchenFlowDbContext>();
-    // await dbContext.Database.MigrateAsync();
-    // logger.LogInformation("Migrations aplicadas com sucesso!");
+    // TODO: Quando o DbContext estiver disponível, implementar:
+    // - Configurar serviços (DbContext, etc.)
+    // - Executar migrations: await dbContext.Database.MigrateAsync();
+    // - Logging apropriado
     
-    logger.LogInformation("Migrator executado com sucesso (sem migrations para aplicar ainda)");
+    Console.WriteLine("Migrator executado com sucesso (sem migrations para aplicar ainda)");
     return 0;
 }
 catch (Exception ex)
 {
-    logger.LogError(ex, "Erro ao executar migrations");
+    Console.WriteLine($"Erro ao executar migrations: {ex.Message}");
     return 1;
 }
